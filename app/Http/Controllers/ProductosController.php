361f355b7b974188bd595885c37abc90aa8c88de
Jean Gonzales <djeangonzalez@gmail.com>
2025-11-24 14:34:34 -0600

    Update ProductosController.php

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -12,9 +12,9 @@ class ProductosController extends Controller
 public function productos()
 {
     $categorias = Categoria::withCount('productos')->get();
-    $productos = Producto::with('categoria')->paginate(6);
+    $productoss = Producto::with('categoria')->paginate(6);
 
-    return view('productos', compact('categorias', 'productos'));
+    return view('productos', compact('categorias', 'productoss'));
 }
 
 }
